9efbf0354f40e9c1f5ea546e08028d4399ff3420
Haroen Viaene <hello@haroen.me>
2016-04-15 13:07:57 +0200

    done with silex

--- serverside/labo01/silex/app/app.php
+++ serverside/labo01/silex/app/app.php
@@ -1,9 +1,6 @@
 <?php
 require __DIR__ . '/bootstrap.php';
 
-
-
-
 $app->get('/', function(Silex\Application $app) {
   $events = $app['db']->fetchAll('SELECT * from concerts ORDER BY start_date');
   return $app['twig']->render('main.twig', array(
@@ -12,6 +9,7 @@ $app->get('/', function(Silex\Application $app) {
     'search' => ''
   ));
 })->bind('homepage');
+
 $app->get('/search/', function(Silex\Application $app) {
   $search = isset($_GET['q']) ? $_GET['q'] : '';
   $events = $app['db']->fetchAll('SELECT * FROM concerts WHERE title LIKE ? ORDER BY start_date', array('%'.$search.'%'));
@@ -21,10 +19,9 @@ $app->get('/search/', function(Silex\Application $app) {
     'search' => $search
   ));
 });
+
 $app->get('/switch/{term}/', function(Silex\Application $app, $term) {
   $dir = '/ss2/labo01/silex/public_html/';
   $app['db']->executeUpdate('UPDATE concerts SET fav = (1 - fav) WHERE id = ?', array($app->escape($term)));
-  // header('Location: '.$dir);
-  return '';
-  // redirect
+  return $app->redirect($app['url_generator']->generate('homepage'));
 });
