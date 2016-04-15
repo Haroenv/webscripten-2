dc1b367184b588ba50760c195d655d6e49d708fd
Haroen Viaene <hello@haroen.me>
2016-04-15 11:03:21 +0200

    working silex

--- serverside/labo01/silex/app/app.php
+++ serverside/labo01/silex/app/app.php
@@ -1,15 +1,10 @@
 <?php
 require __DIR__ . '/bootstrap.php';
 $app->get('/', function(Silex\Application $app) {
-    return 'ohai';
-});
-$app->get('/dikke-memes/', function(Silex\Application $app) {
-    return 'bram';
-});
-
-$app->get('/hello/{name}', function ($name) use ($app) {
   $events = $app['db']->fetchAll('SELECT * from concerts');
-  return $app['twig']->render('hello.twig', array(
+  return $app['twig']->render('main.twig', array(
     'name' => $name,
+    'events' => $events,
+    'search' => $search
   ));
 });
--- serverside/labo01/silex/app/bootstrap.php
+++ serverside/labo01/silex/app/bootstrap.php
@@ -3,11 +3,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/../views',
 ));
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
-        'driver'   => 'pdo_sqlite',
         'path'     => __DIR__ .  DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app.db',
+        'driver'    => 'pdo_mysql',
+        'host'      => 'localhost',
+        'dbname'    => 'concerts',
+        'user'      => 'root',
+        'password'  => 'root',
+        'charset'   => 'utf8mb4'
     )
 ));
