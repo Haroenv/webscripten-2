89edde6398a6ac62b4962444da3b8fdd833a441f
Haroen Viaene <hello@haroen.me>
2016-04-15 11:11:23 +0200

    cool

--- serverside/labo01/silex/app/app.php
+++ serverside/labo01/silex/app/app.php
@@ -1,10 +1,18 @@
 <?php
 require __DIR__ . '/bootstrap.php';
 $app->get('/', function(Silex\Application $app) {
-  $events = $app['db']->fetchAll('SELECT * from concerts');
+  $events = $app['db']->fetchAll('SELECT * from concerts ORDER BY start_date');
   return $app['twig']->render('main.twig', array(
     'name' => $name,
     'events' => $events,
-    'search' => $search
+    'search' => ''
+  ));
+});
+$app->get('/search/{term}/', function(Silex\Application $app, $term) {
+  $events = $app['db']->fetchAll('SELECT * FROM concerts WHERE title LIKE ? ORDER BY start_date', array('%'.$app->escape($term).'%'));
+  return $app['twig']->render('main.twig', array(
+    'name' => $name,
+    'events' => $events,
+    'search' => $app->escape($term)
   ));
 });
