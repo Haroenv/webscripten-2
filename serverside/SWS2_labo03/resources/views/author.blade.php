3ce7cf7001247d47373285cb6bcf2547764d4606
Haroen Viaene <hello@haroen.me>
2016-04-29 12:01:31 +0200

    some finishing touches

---
+++ serverside/SWS2_labo03/resources/views/author.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+@section('title','My Blog')
+@section('content')
+  Author
+@endsection
--- serverside/SWS2_labo03/resources/views/post.blade.php
+++ serverside/SWS2_labo03/resources/views/post.blade.php
@@ -1,14 +1,16 @@
 @extends('master')
-@section('title','My Blog &raquo; Ut placeat …')
+@section('title')
+My Blog &raquo; {{$post->title}}
+@endsection
 @section('content')
-    <p class="breadcrumb"><a href="/">My Blog</a> &raquo; Blogposts</p> &raquo; <a href="/">Blogposts</a> &raquo; Ut placeat dignissimos vero magni qui soluta.</p>
+    <p class="breadcrumb"><a href="/">My Blog</a> &raquo; Blogposts</p> &raquo; <a href="/">Blogposts</a> &raquo; {{$post->title}}</p>
 
     <section class="blogpost">
         <article>
-            <h1>Ut placeat dignissimos vero magni qui soluta.</h1>
-            <footer><p>Written by <em><a href="/authors/3">Jan Janssens</a></em> on <em>2010-04-30 14:34:14</em></p></footer>
+            <h1>{{$post->title}}</h1>
+            <footer><p>Written by <em><a href="/authors/{{$post->author_id}}">{{$post->firstname}} {{$post->lastname}}</a></em> on <em>{{$post->created_at}}</em></p></footer>
             <blockquote>{!! $post->content !!}</blockquote>
-            <p>Comments: <a href="#">2</a></p>
+            <p>Comments: <a href="comments">{{$post->numcomments}}</a></p>
         </article>
     </section>
     <p><a href="/">&larr; Back to blog overview</a></p>
